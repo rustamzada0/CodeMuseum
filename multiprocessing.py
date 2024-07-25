39b6e5d76ed59d3c013b04dd92140b555d15d0f1
rustamzada0 <rustemzade.tunay.2000@gmail.com>
2024-07-25 14:49:15 +0400

    Update

--- multiprocessing.py
+++ multiprocessing.py
@@ -39,8 +39,8 @@ def threading_example():
     n = 10**8
     start_time = time.time()
     
-    thread1 = threading.Thread(target=cpu_bound_task, args=(n,))
-    thread2 = threading.Thread(target=cpu_bound_task, args=(n,))
+    thread1 = threading.Thread(target=cpu_bound_task, args=(n,)) # bir proses texmini 5 saniyelik is gorur ve thread islederek "2" dene 5 saniyelik isi eyni anda gordursek de yene de 5 saniye etmeli idi ama GIL icaze vermir ve python kodu sira ile icra olunur (1 thread ile)
+    thread2 = threading.Thread(target=cpu_bound_task, args=(n,)) # bir proses texmini 5 saniyelik is gorur ve thread islederek "2" dene 5 saniyelik isi eyni anda gordursek de yene de 5 saniye etmeli idi ama GIL icaze vermir ve python kodu sira ile icra olunur (1 thread ile)
     
     # coxlu thread baslatsaq da GIL pythonda ancag 1 thread icaze verir
     thread1.start()
@@ -59,3 +59,36 @@ threading_example()
 # C++ , C, C# ve Java: Evet, aynı anda bir kodu (örneğin 5 thread ile) okuyup işleyebilirsiniz.
 # Python: Hayır, GIL nedeniyle aynı anda birden fazla thread Python kodunu okuyup işleyemez.
 # Evet, iki thread'in de çalıştığını görüyor olmanız normaldir, çünkü Python'da threading modülü ile thread'ler gerçekten çalışır. Ancak, GIL nedeniyle aynı anda yalnızca bir thread'in Python bytecode'u çalıştırmasına izin verilir. Bu nedenle, thread'ler arasında bağlam değişimi (context switching) yapılarak çalışırlar, fakat gerçek paralelizm sağlanmaz.
+
+# import threading
+# import time
+
+# def cpu_bound_task():
+#     x = 0
+#     while x < 10**8:
+#         x += 1
+
+# def cpu_bound_task2():
+#     x = 0
+#     while x < 10**8:
+#         x += 1
+
+# time1 = time.time()
+# thread1 = threading.Thread(target=cpu_bound_task) # bir proses texmini 5 saniyelik is gorur ve thread islederek "2" dene 5 saniyelik isi eyni anda gordursek de yene de 5 saniye etmeli idi ama GIL icaze vermir ve python kodu sira ile icra olunur (1 thread ile)
+# thread2 = threading.Thread(target=cpu_bound_task2) # bir proses texmini 5 saniyelik is gorur ve thread islederek "2" dene 5 saniyelik isi eyni anda gordursek de yene de 5 saniye etmeli idi ama GIL icaze vermir ve python kodu sira ile icra olunur (1 thread ile)
+
+# thread1.start()
+# # thread2.start()
+
+# thread1.join()
+# # thread2.join()
+
+# time2 = time.time()
+# print(f"Threading CPU time: {time2 - time1} saniye")
+
+# print("Both threads have finished execution.")
+
+
+# ama multiprocessing ile çoklu thredlerle aynı anda Python kodu okuyup işleyebilirsiniz. 
+# thread1 = multiprocessing.Process(target=cpu_bound_task)
+# thread2 = multiprocessing.Process(target=cpu_bound_task2)
---
+++ withopen/main.py
@@ -0,0 +1,41 @@
+# f = open("example.txt")
+
+# content = f.read()
+
+# f.seek(0)
+# content2 = f.readline()
+
+# f.seek(0)
+# content3 = f.readlines()
+# print(f.tell()) #kursorun yeri
+
+# # print(content)
+# print(content2)
+# print(content3)
+
+# f.close()
+
+
+# with open('example.txt', 'w') as file:
+#     file.write("Bu, file-a yazdığımız ilk setir.\n")
+#     file.write("Bu da ikinci setir.\n")
+
+# with open('example.txt', 'r') as file:
+#     content = file.read()
+#     print(content)
+
+# with open('example.txt', 'a') as file:
+#     file.write("Bu, file-a yazdığımız son setir.\n")
+
+
+# import os
+# if os.path.exists("example.txt"):
+#   os.remove("example.txt")
+# else:
+#   print("The file does not exist")
+
+# import os
+# os.rmdir("myfolder")
+
+# import os
+# print(os.listdir())
