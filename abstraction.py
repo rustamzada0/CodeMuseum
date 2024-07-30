893b5ea5980cc0e157aee66bd2fd838fe943b85a
rustamzada0 <rustemzade.tunay.2000@gmail.com>
2024-07-31 03:38:33 +0400

    Update!

--- abstraction.py
+++ abstraction.py
@@ -42,3 +42,64 @@ print("Circle Perimeter:", circle.perimeter())
 
 print("Square Area:", square.area())
 print("Square Perimeter:", square.perimeter())
+
+
+
+
+
+# from abc import ABC, abstractmethod
+
+# class Animal(ABC):
+#     family = "Animal"
+    
+#     # constructor
+#     def __init__(self, name, breed, color, gender, size):
+#         self.n = name
+#         self.b = breed
+#         self.c = color
+#         self.g = gender
+#         self.s = size
+
+#     def __str__(self):
+#         return self.n
+    
+#     @abstractmethod
+#     def get_info(self):
+#         return self.n, self.b, self.c, self.g, self.s
+    
+#     @classmethod
+#     def get_family(cls):
+#         return cls.family
+        
+
+# class Dog(Animal):
+#     family = "Dog"
+
+#     def get_info(self):
+#         return super().get_info()
+
+
+# class Cat(Animal):
+#     family = "Cat"
+
+#     def __init__(self, name, breed, color, gender, size, hairy=True):
+#         super().__init__(name, breed, color, gender, size)
+#         self.hairy = hairy
+
+
+#     def get_info(self):
+#         return super().get_info(), self.hairy
+
+# class Bird(Animal):
+#     pass
+#     # def get_info(self):
+#     #     return super().get_info()
+
+# # animal1 = Animal("Jack", "African", "White", "Female", "Small")
+# # print(animal1)
+
+# cat1 = Cat("Jack", "African", "White", "Female", "Small")
+# print(cat1.get_info())
+
+# bird1 = Bird("Jack", "African", "White", "Female", "Small")
+# print(bird1)
